d5b0d3ac4f30e6b10a6ef2635bf7ac136e8307d8
Dillip Kumar Sahu <sahudillipkumar466@gmail.com>
2023-04-30 23:24:37 +0530

    firestore added to store data

--- Week-13/shortly/src/components/ListItem.jsx
+++ Week-13/shortly/src/components/ListItem.jsx
@@ -1,9 +1,14 @@
 
-
+import copy from "copy-to-clipboard";
 const ListItem = ({ item, index, dataList, setDataList }) => {
 
     const handleDelete = () => {
         setDataList(dataList.filter((e, i) => i !== index))
+    }
+
+    const copyToClipboard = () => {
+        copy(item.short_link);
+        alert(`You have copied "${item.short_link}"`);
     }
 
     return (
@@ -12,7 +17,7 @@ const ListItem = ({ item, index, dataList, setDataList }) => {
                 <h5>Original Link - <a href={item.original_link} target="_blank">{item.original_link}</a></h5>
                 <h5>Short Link - <a href={'https://' + item.short_link} target="_blank">https://{item.short_link}</a></h5>
                 <div>
-                    <button>Edit</button>
+                    <button onClick={copyToClipboard}>Copy link</button>
                     <button onClick={handleDelete}>Delete</button>
                 </div>
             </li>
